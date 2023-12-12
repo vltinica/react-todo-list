b411e91fb56e2c460649aa7b462e131dad0b1c17
Vladimir <vltinica@gmail.com>
2023-12-11 19:16:18 -0500

    logical conclusion of Todo.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 import "./App.css";
 import TodoForm from "./components/Todos/TodoForm";
 import { TodoList } from "./components/Todos/TodoList";
@@ -7,17 +8,37 @@ function App() {
   const [todos, setTodos] = useState([]);
 
   const addTodoHandler = (text) => {
-    setTodos([...todos, text]);
+    const newTodo = {
+      text: text,
+      isCompleted: false,
+      id: uuidv4(),
+    };
+    setTodos([...todos, newTodo]);
   };
 
-  const deleteTodoHandler = (index) => {
-    setTodos(todos.filter((_, inx) => inx !== index));
+  const deleteTodoHandler = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
   };
+
+  const toggleTodoHandler = (id) => {
+    setTodos(
+      todos.map((todo) => {
+        return todo.id === id
+          ? { ...todo, isCompleted: !todo.isCompleted }
+          : { ...todo };
+      })
+    );
+  };
+
   return (
     <div className="App">
       <h1>Todo App</h1>
       <TodoForm addTodo={addTodoHandler} />
-      <TodoList todos={todos} deleteTodo={deleteTodoHandler} />
+      <TodoList
+        todos={todos}
+        deleteTodo={deleteTodoHandler}
+        toggleTodo={toggleTodoHandler}
+      />
     </div>
   );
 }
--- src/components/Todos/TodoList.js
+++ src/components/Todos/TodoList.js
@@ -2,12 +2,17 @@ import React from "react";
 import Todo from "./Todo";
 import styles from "./TodoList.module.css";
 
-export const TodoList = ({ todos, deleteTodo }) => {
+export const TodoList = ({ todos, deleteTodo, toggleTodo }) => {
   return (
     <div className={styles.todoListContainer}>
       {!todos.length && <h2>Todo list is empty</h2>}
-      {todos.map((todo, index) => (
-        <Todo key={index} todo={todo} index={index} deleteTodo={deleteTodo} />
+      {todos.map((todo) => (
+        <Todo
+          key={todo.id}
+          todo={todo}
+          deleteTodo={deleteTodo}
+          toggleTodo={toggleTodo}
+        />
       ))}
     </div>
   );
